dd1cc087cf4fe1865e3a944b30eb7a5dab805642
Alex2389ru <laurov966@yandex.ru>
2025-04-27 14:29:20 +0400

    Add files via upload

---
+++ three.py
@@ -0,0 +1,7 @@
+user1 = int(input('Введите число'))
+user2 = int(input( 'Введите число'))
+
+if user1 < user2:
+   print('user1 < user2') 
+else:
+   print('user1 > user2')      
---
+++ two.py
@@ -0,0 +1,8 @@
+user = int(input('Введите возраст'))
+age = 18
+
+if user >= age:
+    print('Входи')
+else:
+    print('Вход запрещен')
+  
