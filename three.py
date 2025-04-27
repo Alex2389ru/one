320092482861b3ad1a56c0ffa14cb5b3deb33ae4
Alex2389ru <laurov966@yandex.ru>
2025-04-27 16:36:26 +0400

    Update three.py

--- three.py
+++ three.py
@@ -1,7 +1,9 @@
-user1 = int(input('Введите число'))
-user2 = int(input( 'Введите число'))
+user1 = int(input('Пользователь 1 Введите число'))
+user2 = int(input( 'пользователь 2 Введите число'))
 
-if user1 < user2:
-   print('user1 < user2') 
+if user1 > user2:
+   print('user1')
+elif user1 < user2:
+   print('user2')       
 else:
-   print('user1 > user2')      
+   print('Числа равны')  
